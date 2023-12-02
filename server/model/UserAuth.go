9765734b548a65876c362911caaaf4ef36c17e4c
raein11 <raein11@foxmail.com>
2023-12-02 14:52:35 +0800

    add model

---
+++ server/model/UserAuth.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+// 用户身份信息模型
+type UserAuth struct {
+	Id            int64     `json:"id"`
+	UserInfoId    string    `json:"user_info_id"`
+	Username      string    `json:"username"`
+	Password      string    `json:"password"`
+	LoginType     string    `json:"login_type"`
+	LastLoginTime string    `json:"last_login_time"`
+	IpAddress     string    `json:"ip_address"`
+	IpSource      string    `json:"ip_source"`
+	CreateTime    time.Time `json:"create_time"`
+	UpdateTime    time.Time `json:"update_time"`
+}

c74342dbf64d8c5b58cf3c4b991160880a414b44
raein11 <raein11@foxmail.com>
2023-12-09 17:03:38 +0800

    add utils

---
+++ server/utils/gin_context.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"blog-server/utils/r"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+// JSON 绑定
+func BindJSON[T any](context *gin.Context) (data T) {
+	err := context.ShouldBindJSON(&data)
+	if err != nil {
+		log.Fatal("BindJSON：", err)
+	}
+	return
+}
+
+// 参数合法性校验
+func Validate(context *gin.Context, data any) {
+	validateAns := Validator.Validate(data)
+	if validateAns != "" {
+		r.ReturnJson(context, 200, 400, validateAns, nil)
+		panic(nil)
+	}
+}
+
+// JSON 绑定验证 + 合法性校验
+func BindJSONAndValid[T any](context *gin.Context) (data T) {
+	err := context.ShouldBindJSON(&data)
+	if err != nil {
+		log.Fatal("BindJSON：", err)
+	}
+	Validate(context, &data)
+	return data
+}
---
+++ server/utils/validator.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
+
+var Validator = new(ValidateUtil)
+
+type ValidateUtil struct{}
+
+var (
+	validate *validator.Validate
+)
+
+func (v *ValidateUtil) Validate(data any) string {
+	ans := ""
+	validate = validator.New(validator.WithRequiredStructEnabled())
+	err := validate.Struct(data)
+	if err != nil {
+		fmt.Println("validate err: ", err)
+		for _, e := range err.(validator.ValidationErrors) {
+			ans += fmt.Sprintf("Field %s, Error Tag %s, Error Param %s\n", e.Field(), e.Tag(), e.Param())
+		}
+	} else {
+		fmt.Println("validate success")
+	}
+	return ans
+}

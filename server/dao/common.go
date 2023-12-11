53260e50d8566f5038716682df40486e53b1d065
raein11 <raein11@foxmail.com>
2023-12-11 21:05:04 +0800

    fix dao common

--- server/dao/common.go
+++ server/dao/common.go
@@ -1,15 +1,15 @@
 package dao
 
 import (
-	"blog-server/router"
+	"gorm.io/gorm"
 	"log"
 )
 
-var db = router.DB
+var DB *gorm.DB
 
 // Create 通用创建
 func Create[T any](data *T) {
-	err := db.Create(&data).Error
+	err := DB.Create(&data).Error
 	if err != nil {
 		log.Println("创建失败：", err)
 		panic(err)
@@ -18,7 +18,7 @@ func Create[T any](data *T) {
 
 // GetOne 通用查询单行
 func GetOne[T any](data T, query string, args ...any) T {
-	err := db.Where(query, args...).Limit(1).Find(&data).Error
+	err := DB.Where(query, args...).Limit(1).Find(&data).Error
 	if err != nil {
 		log.Println("查询失败：", err)
 		panic(err)
@@ -30,10 +30,10 @@ func GetOne[T any](data T, query string, args ...any) T {
 func Update[T any](data *T, cols ...string) {
 	// 判断是否传入字段名参数
 	if len(cols) == 0 {
-		db.Model(&data).Updates(&data)
+		DB.Model(&data).Updates(&data)
 		return
 	}
-	err := db.Model(&data).Select(cols).Updates(&data).Error
+	err := DB.Model(&data).Select(cols).Updates(&data).Error
 	if err != nil {
 		log.Println("更新失败：", err)
 		panic(err)
@@ -42,7 +42,7 @@ func Update[T any](data *T, cols ...string) {
 
 // UpdateMap 更新 map
 func UpdateMap[T any](data *T, mp map[string]any, query string, args ...any) {
-	err := db.Model(&data).Where(query, args).Updates(mp).Error
+	err := DB.Model(&data).Where(query, args).Updates(mp).Error
 	if err != nil {
 		log.Println("更新失败：", err)
 		panic(err)
@@ -51,7 +51,7 @@ func UpdateMap[T any](data *T, mp map[string]any, query string, args ...any) {
 
 // Delete 通用删除
 func Delete[T any](data T, query string, args ...any) {
-	err := db.Where(query, args).Delete(&data).Error
+	err := DB.Where(query, args).Delete(&data).Error
 	if err != nil {
 		log.Println("删除失败：", err)
 		panic(err)
